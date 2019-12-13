1d1240e20117cd0a8461f01da013c4af360db735
valterlej <valter.junior@ifpr.edu.br>
2019-12-13 13:28:53 -0300

    example2

--- example2.py
+++ example2.py
@@ -1,14 +1,9 @@
-from itertools import accumulate
-
 from filetree import File
-import cv2
 
-print('Creating a root file loading all subdirectories and files.')
-
+print('Creating a root file and loading all subdirectories and files.')
 
 root = File('/media/valter/Arquivos/Datasets/UCF-101',load=True)
-#root.print_tree()
+root.print_tree()
 
-files = root.get_all_files_from_here(filter='.wei')
-
+files = root.get_all_files_from_here(filter='.avi')
 print(files)
